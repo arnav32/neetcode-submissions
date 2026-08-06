2780e7f32ffa0ad882aed51ce146ed5e6e47f46b
Arnav <65024149+arnav32@users.noreply.github.com>
2026-08-06 22:38:47 +0100

    Add: valid-sudoku - submission-3

---
+++ Data Structures & Algorithms/valid-sudoku/submission-3.py
@@ -0,0 +1,73 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        def validGroup(nums: List[str]) -> bool:
+            dig_map = defaultdict(int)
+            for num in nums:
+                dig_map[num] += 1
+            if '.' in dig_map: del dig_map['.']
+
+            print(dig_map)
+
+            # check for duplicates
+            if set(dig_map.values()) != {1} & set(dig_map.values()) != {}: return False
+
+            # check theyre all between 1-9
+            for key in dig_map:
+                if int(key) not in range(1, 10): return False
+                    
+            return True
+
+        # checking rows
+        for row in board:
+            if not validGroup(row): 
+                print("row")
+                return False
+        
+        # checking cols
+        for col_i in range(9):
+            col = [board[row_i][col_i] for row_i in range(9)]
+            if not validGroup(col): 
+                print("col")
+                return False
+        
+        # checking sub-boxes
+        for box_row in range(3):
+            for box_col in range(3):
+                box = []
+                for row_i in range(box_row*3, box_row*3 + 3):
+                    for col_i in range(box_col*3, box_col*3 + 3):
+                        box.append(board[row_i][col_i])
+                if not validGroup(box):
+                    print(box)
+                    print("boxfail")
+                    
+                    return False
+        
+        return True
+
+
+
+
+        # [0][0]
+        # [0][1]
+        # [0][2]
+        # [1][0]
+        # [1][1]
+        # [1][2]
+        # [2][0]
+        # [2][1]
+        # [2][2]
+
+
+
+        # for middle right box
+
+        # [0][0]
+        # [0][1]
+        # [0][2]
+        # [1][0]
+        # [1][1]
+        # [1][2]
+        # [2][0]
+        # [2][1]
+        # [2][2]
